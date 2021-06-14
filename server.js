4139abdfe3379fedeeb7e5ed88322be60b3882b4
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-06-14 16:00:20 +0300

    adding unhandled error handlers

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+//catching uncaught exceptions to listen at the begining to the whole app
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('uncaught Rejection!💥 Shutting down ...');
+  process.exit(1);
+});
+
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -19,6 +27,17 @@ mongoose
     console.log('DB connection successful');
   });
 
-app.listen(process.env.PORT || 3000, () =>
+const server = app.listen(process.env.PORT || 3000, () =>
   console.log('server started on port 3000')
 );
+
+//handle unhandled rejection
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection!💥 Shutting down ...');
+
+  //give the server time to finish all requests
+  server.close(() => {
+    process.exit(1); //1 for error , 0 for success
+  });
+});
