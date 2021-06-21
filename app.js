a620ba4b52f0b28992e5654e7176f64a5b52690a
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-06-21 12:21:43 +0300

    add helmet to set security http headers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,6 +11,10 @@ const userRouter = require('./routes/userRouter');
 const app = express();
 
 //MIDDLEWARE
+//set security http headers
+app.use(helmet());
+
+//development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 //define how many req by Ip allowed by certin amount of time
@@ -19,10 +24,20 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP please try again in an hour!',
 });
 app.use('/api', limiter); //apply to urls start with api
+
+//allow cors on browser
 app.use(cors({ origin: '*' }));
 
-app.use(express.json());
+//body parser,reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb', //limit body size to 10kb
+  })
+);
+//serving static files
 app.use(express.static(`${__dirname}/public`));
+
+//test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date();
   next();
