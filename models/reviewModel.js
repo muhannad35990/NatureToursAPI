6ba343e2ccc686d22d877d139b5994b73c8cfcc1
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-06-23 11:16:54 +0300

    add static method in the review model to calculate statistics and update the tour fields when create new review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -44,6 +45,34 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name',
   });
   next();
+});
+
+//static method to calculate statistics
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  //using aggregation pipeline
+  //match to get all review belong to tour with that tourId
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour', //group by tour
+        nRating: { $sum: 1 }, //count number of ratings (number of reviews)
+        avgRating: { $avg: '$rating' }, //average of rating
+      },
+    },
+  ]);
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingAverage: stats[0].avgRating,
+    ratingQantity: stats[0].nRating,
+  });
+};
+
+//middleware to update call updating statistic and save to the doc in the Tour
+reviewSchema.post('save', function () {
+  //this points to current review , coz we do not have access yet the the Review Model we use constructor to get around
+  this.constructor.calcAverageRatings(this.tour); //call the static method to calclate
 });
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
