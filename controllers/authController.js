9be9d674a84d1d05444d3777669f30a32dec1653
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-06-24 13:39:40 +0300

    add password reset template to send reset password email

--- controllers/authController.js
+++ controllers/authController.js
@@ -111,17 +111,13 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
 
-  //send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-  const message = `Forgot your password? submit a PATCH request with your new password and password confirm to : ${resetURL}. \n If you didn't forgot your password, Please ignore this email`;
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: 'Your password reset token (valid for 10 min)',
-    //   message,
-    // });
+    //send it to user's email
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    await new Email(user, resetURL).sendPasswordReset();
     res.status(200).json({ status: 'success', message: 'Token sent to email' });
   } catch (err) {
     user.passwordRestToken = undefined;
